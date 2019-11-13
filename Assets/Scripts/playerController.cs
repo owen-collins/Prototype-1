7c97bac5e2a6259cc4bc8370a045008428518220
Owen Collins <56165499+owen-collins@users.noreply.github.com>
2019-11-13 08:21:40 -0800

    Changes from last class

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
+    public float turnSpeed;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle foward
-        transform.Translate(Vector3.forward*Time.deltaTime *speed);
-    }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);}
 }
